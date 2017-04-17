6034903983abc69e85b82b8fcc713b718d7833d2
Kseniya <ksu.nyaka@gmail.com>
2017-04-17 17:40:12 +0800

    Working version of LSTM for MBTI profiling

--- source/get_data.py
+++ source/get_data.py
@@ -1,9 +1,10 @@
 from pymongo import MongoClient
 import numpy as np
+import random
 
 ###
 MONGO_PORT = 27017
-MONGO_HOST = '172.29.29.24'
+MONGO_HOST = '172.29.30.160'
 ###
 
 
@@ -39,7 +40,7 @@ def get_period_data(n_periods, modality, db):
     return output
 
 
-def input_output_generation(period_features, groundtruth_collection_name, db, mbti_position):
+def input_output_generation(period_features, n_periods, groundtruth_collection_name, db, mbti_position):
     # get data from mongo and prepare it for neural network format
     db = connect_to_database_books_collection(MONGO_HOST, MONGO_PORT, db)
     # mbti_position is what letter in mbti profile we want to predict
@@ -47,13 +48,14 @@ def input_output_generation(period_features, groundtruth_collection_name, db, mb
     output_data = list()
     #input data - list of lists of 53-dimensional vectors, one for each period
     input_data = list()
+    for p in range(0, n_periods):
+        input_data.append(list())
 
+    for period in range(0, n_periods):
+        for user in period_features:
+            input_data[period].append(period_features[user][period])
     for user in period_features:
         user_data = db[groundtruth_collection_name].find_one({'twitterUserName': user})
-        user_features = list()
-        for features in period_features[user]:
-            user_features.append(features)
-        input_data.append(user_features)
         output_data.append(convert_mbti_to_vector(user_data['mbti'], mbti_position))
 
     return input_data, output_data
@@ -64,39 +66,58 @@ def convert_mbti_to_vector(mbti, mbti_position):
     # I S T J -> 0
     # E N F P -> 1
     mbti_vector = list()
-    if mbti[0] == 'I': mbti_vector.append(0)
-    if mbti[0] == 'E': mbti_vector.append(1)
-    if mbti[1] == 'S': mbti_vector.append(0)
-    if mbti[1] == 'N': mbti_vector.append(1)
-    if mbti[2] == 'T': mbti_vector.append(0)
-    if mbti[2] == 'F': mbti_vector.append(1)
-    if mbti[3] == 'J': mbti_vector.append(0)
-    if mbti[3] == 'P': mbti_vector.append(1)
+    if mbti[0] == 'I':
+        mbti_vector.append([0, 1])
+    if mbti[0] == 'E':
+        mbti_vector.append([1, 0])
+    if mbti[1] == 'S':
+        mbti_vector.append([0, 1])
+    if mbti[1] == 'N':
+        mbti_vector.append([1, 0])
+    if mbti[2] == 'T':
+        mbti_vector.append([0, 1])
+    if mbti[2] == 'F':
+        mbti_vector.append([1, 0])
+    if mbti[3] == 'J':
+        mbti_vector.append([0, 1])
+    if mbti[3] == 'P':
+        mbti_vector.append([1, 0])
 
-    output = mbti_vector[mbti_position]
-    output_vector = [output]
-
-    if output == 0: output_vector.append(1)
-    else: output_vector.append(0)
-
-    return output_vector
+    return mbti_vector[mbti_position]
 
 
-def split_data_to_train_test(input_data, output_data, threshold = 0.8):
-    random_state = np.random.rand(len(input_data))
+def split_data_to_train_test(input_data, output_data, n_periods, threshold = 0.9):
+    random_state = np.random.rand(len(input_data[0]))
 
     input_train, output_train, input_test, output_test = list(), list(), list(), list()
 
     for i in range(1, len(random_state)):
+        period_features = list()
+        for p in range(0, n_periods):
+            period_features.append(input_data[p][i])
         if random_state[i] < threshold:
-            input_train.append(input_data[i])
+            input_train.append(period_features)
             output_train.append(output_data[i])
         else:
-            input_test.append(input_data[i])
+            input_test.append(period_features)
             output_test.append(output_data[i])
 
     return input_train, output_train, input_test, output_test
 
+
+def get_batch(input_data, output_data, batch_size):
+    indexes = random.sample(range(0, len(input_data)), batch_size)
+    input_batch, output_batch = list(), list()
+
+    for i in indexes:
+        input_batch.append(input_data[i])
+        output_batch.append(output_data[i])
+
+    return input_batch, output_batch
+
+
+
 # period_data = get_period_data(2, 'tweets', 'tweets_10_periods')
-# input_data, output_data = input_output_generation(period_data, 'users', 'user-profiling', 1)
-# train_input, train_output, test_input, test_output = split_data_to_train_test(input_data, output_data)
+# input_data, output_data = input_output_generation(period_data, 2, 'users', 'user-profiling', 1)
+# train_input, train_output, test_input, test_output = split_data_to_train_test(input_data, output_data, 2)
+# i = 0
--- source/tutorial.py
+++ source/tutorial.py
@@ -5,77 +5,75 @@ from tensorflow.contrib import rnn
 from pymongo import MongoClient
 import get_data
 import numpy as np
+from tensorflow.python import debug as tf_debug
 
-###
-MONGO_PORT = 27017
-MONGO_HOST = '172.29.29.24'
-MONGO_DB_NAME = 'user-profiling'
-###
 
 # Parameters
 learning_rate = 0.001
-training_iters = 50000
+training_iters = 5000
 display_step = 1000
-batch_size = 2
-features_dimensions = 53
-# number of units in RNN cell
-n_hidden = 512
+input_dimension = 53
+output_dimension = 2
+n_hidden = 256
+n_input = 5
+batch_size = 32
 
-
-
-x = tf.placeholder(tf.float32, [None, batch_size, features_dimensions])
-y = tf.placeholder(tf.float32, [None, 2])
+x = tf.placeholder(tf.float32, [None, n_input, input_dimension])
+y = tf.placeholder(tf.float32, [None, output_dimension])
 
 
 # RNN output node weights and biases
 weights = {
-    'out': tf.Variable(tf.random_normal([n_hidden, 2]))
+    'out': tf.Variable(tf.random_normal([n_hidden, output_dimension]))
 }
 biases = {
-    'out': tf.Variable(tf.random_normal([2]))
+    'out': tf.Variable(tf.random_normal([output_dimension]))
 }
 
 
 def RNN(x, weights, biases):
     rnn_cell = rnn.BasicLSTMCell(n_hidden)
-    # Generate a n_input-element sequence of inputs
-    # (eg. [had] [a] [general] -> [20] [6] [33])
-    # generate prediction
-    outputs, states = tf.nn.dynamic_rnn(rnn_cell, x, dtype=tf.float32)
+    rnn_cell = rnn.MultiRNNCell([rnn_cell] * 2)
+    rnn_cell = rnn.DropoutWrapper(rnn_cell, 0.8)
+    x = tf.unstack(x, n_input, 1)
+    outputs, states = rnn.static_rnn(rnn_cell, x, dtype=tf.float32)
     # there are n_input outputs but
     # we only want the last output
     return tf.matmul(outputs[-1], weights['out']) + biases['out']
 
 pred = RNN(x, weights, biases)
-print (pred)
 
 # Loss and optimizer
 cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=pred, labels=y))
 optimizer = tf.train.RMSPropOptimizer(learning_rate=learning_rate).minimize(cost)
-
 # Model evaluation
-correct_pred = tf.equal(pred, y)
-accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(pred, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 # Initializing the variables
 init = tf.global_variables_initializer()
 
 
-period_data = get_data.get_period_data(2, 'tweets', 'tweets_10_periods')
-input_data, output_data = get_data.input_output_generation(period_data, 'users', 'user-profiling', 1)
-train_input, train_output, test_input, test_output = get_data.split_data_to_train_test(input_data, output_data)
+period_data = get_data.get_period_data(n_input, 'tweets', 'tweets_10_periods')
+input_data, output_data = get_data.input_output_generation(period_data, n_input, 'users', 'user-profiling', 0)
+train_input, train_output, test_input, test_output = get_data.split_data_to_train_test(input_data, output_data, n_input)
 with tf.Session() as session:
+    # session = tf_debug.LocalCLIDebugWrapperSession(session)
+    # session.add_tensor_filter("has_inf_or_nan", tf_debug.has_inf_or_nan)
     session.run(init)
     step = 0
     loss_total = 0
     acc_total = 0
+    offset = 0
     while step < training_iters:
-        _, acc, loss, pred = session.run([optimizer, accuracy, cost, pred], \
-                                                feed_dict={x: train_input, y: train_output})
+        input_batch, output_batch = get_data.get_batch(train_input, train_output, batch_size)
+        _, acc, loss, prediction = session.run([optimizer, accuracy, cost, pred], \
+                                                feed_dict={x: input_batch, y: output_batch})
 
         print("Iter= " + str(step + 1) + ", Average Loss= " + \
               "{:.6f}".format(loss) + ", Average Accuracy= " + \
               "{:.2f}%".format(100 * acc))
+        step += 1
 
     incorrect = session.run(accuracy,{x: test_input, y: test_output})
     print('Epoch {:2d} error {:3.1f}%'.format(training_iters + 1, 100 * incorrect))
