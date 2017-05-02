3e92235c825a3055d71bfdeca51919cb662fac0a
Kseniya <ksu.nyaka@gmail.com>
2017-05-02 15:17:13 +0800

    NMF added, store in one vector added

--- source/get_data.py
+++ source/get_data.py
@@ -1,21 +1,27 @@
 from pymongo import MongoClient
 import numpy as np
-import numpy
 import random
 from sklearn.decomposition import NMF
+import logging
+import hashlib
 
 ###
+# Mongo params
 MONGO_PORT = 27017
 MONGO_HOST = 'localhost'
 MONGO_DB = 'mbti_research'
-###
+MONGO_DB_RESTORE = MONGO_DB + '_restore'
 # Features dimensions
 TEXT_DIM = 53
 LIWC_DIM = 64
 LDA_DIM = 50
 LOCATION_DIM = 886
-#
+MEDIA_DIM = 1000
+PERIODS = 10
+###
 features_dim = TEXT_DIM + LIWC_DIM + LDA_DIM + LOCATION_DIM
+logging.basicConfig(filename='get_data.log', level=logging.DEBUG)
+hash_maker = hashlib.md5()
 
 
 def connect_to_database(host, port, db_name):
@@ -56,7 +62,7 @@ def get_period_data(n_periods, features_types, db, features_dim):
                     add = False
             if add:
                 output[user] = periods_data[user]
-    print("Find {%d} users in {%d} periods" % (len(output), n_periods))
+    logging.info("Find {%d} users in {%d} periods" % (len(output), n_periods))
     return output
 
 
@@ -136,52 +142,14 @@ def get_batch(input_data, output_data, batch_size):
     return input_batch, output_batch
 
 
-"""
-@INPUT:
-    R     : a matrix to be factorized, dimension N x M
-    P     : an initial matrix of dimension N x K
-    Q     : an initial matrix of dimension M x K
-    K     : the number of latent features
-    steps : the maximum number of steps to perform the optimisation
-    alpha : the learning rate
-    beta  : the regularization parameter
-@OUTPUT:
-    the final matrices P and Q
-"""
-
-
-def matrix_factorization(R, P, Q, K, steps=5000, alpha=0.0002, beta=0.02):
-    Q = Q.T
-    for step in range(steps):
-        if step % 100 == 0:
-            print ("{%d} step of NMF" % step)
-        for i in range(len(R)):
-            for j in range(len(R[i])):
-                if R[i][j] > 0:
-                    eij = R[i][j] - numpy.dot(P[i, :], Q[:, j])
-                    for k in range(K):
-                        P[i][k] = P[i][k] + alpha * (2 * eij * Q[k][j] - beta * P[i][k])
-                        Q[k][j] = Q[k][j] + alpha * (2 * eij * P[i][k] - beta * Q[k][j])
-        eR = numpy.dot(P, Q)
-        e = 0
-        for i in range(len(R)):
-            for j in range(len(R[i])):
-                if R[i][j] > 0:
-                    e = e + pow(R[i][j] - numpy.dot(P[i, :], Q[:, j]), 2)
-                    for k in range(K):
-                        e = e + (beta / 2) * (pow(P[i][k], 2) + pow(Q[k][j], 2))
-        if e < 0.001:
-            break
-    return P, Q.T
-
-
 def fill_missed_modality(period):
     # get all users with activity in current period
     # build the matrix and do it factorization
     # need to know the order of features to write them into database then
+    # store all features in one vector as a result
     db = connect_to_database(MONGO_HOST, MONGO_PORT, MONGO_DB)
-    modalities = ['text', 'liwc']  # , 'lda', 'location']
-    modalities_dimensions = [53, 64]  # , 50, 886]
+    modalities = ['text', 'liwc', 'lda', 'location', 'media']
+    modalities_dimensions = [53, 64, 50, 886, 1000]
     period_users = list()  # all active users in any social network for this period
     for modality in modalities:
         users = db['MBTI_%d_%s' % (period, modality)].distinct('_id')
@@ -190,59 +158,123 @@ def fill_missed_modality(period):
                 period_users.append(user)
 
     users_features = list()  # the order of users will be the same as in users list
-    incomplete_users = 0
+    incomplete_dimensions = 0
     for user in period_users:
         feature_vector = list()
         for modality, dimension in zip(modalities, modalities_dimensions):
             features = db['MBTI_%d_%s' % (period, modality)].find_one({"_id": user})
             if features is None:
                 feature_vector.extend([0] * dimension)
-                incomplete_users += 1
+                incomplete_dimensions += 1
             else:
-                if len(features) != dimension + 1:
-                    print("Error for user {%s}, modality {%s} dimension. Need {%d}, found {%d}"
-                          % (user, modality, dimension + 1, len(features)))
                 for feature_name in features:
                     if feature_name != '_id':
-                        if (features[feature_name] < 0):  # FIXME it happens only for sentiment score here
+                        if features[feature_name] < 0:  # FIXME it happens only for sentiment score here
                             feature_vector.append((-1.0) * features[feature_name])
                         else:
                             feature_vector.append(features[feature_name])
         users_features.append(np.array(feature_vector))
 
-    print("Found {%d} incomplete users" % incomplete_users)
+    logging.info("Found {%d} incomplete dimensions" % incomplete_dimensions)
     R = np.array(users_features)
-    N = len(users_features)
-    M = len(users_features[0])
-    K = features_dim
+    model = NMF(init='random', random_state=0)
+    W = model.fit_transform(R)
+    H = model.components_
+    transformed_data = np.dot(W, H)
 
-    P = np.random.rand(N, K)
-    Q = np.random.rand(M, K)
-
-    nP, nQ = matrix_factorization(R, P, Q, K)
-    transformed_data = np.dot(nP, nQ.T)
-    # model = NMF(init='random')
-    # W = model.fit_transform(R)
-    # H = model.components_
-    # transformed_data = np.dot(W, H)
-
-    db_restore = connect_to_database(MONGO_HOST, MONGO_PORT, 'mbti_research_restore')
+    db_restore = connect_to_database(MONGO_HOST, MONGO_PORT, MONGO_DB_RESTORE)
     for user in period_users:
         sum_dimension = 0
         user_position = period_users.index(user)
         for modality, dimension in zip(modalities, modalities_dimensions):
-            features = db['MBTI_%d_%s' % (period, modality)].find_one({"_id": user})
+            features = db['MBTI_%d_%s' % (period, modality)].find_one({'_id': user})
             if features is None:
-                new_features = transformed_data[user_position][sum_dimension:sum_dimension + dimension]
                 features_to_db = dict()
-                features_to_db["_id"] = user
-                for j in range(0, len(new_features)):
-                    features_to_db[str(j)] = new_features[j]
+                features_to_db['_id'] = user
+                for j in range(0, dimension):
+                    features_to_db['%s_%d' % (modality, j + 1)] = \
+                        transformed_data[user_position][sum_dimension + j]
                 db_restore['MBTI_%d_%s' % (period, modality)].insert(features_to_db)
-            sum_dimension += dimension + 1
+                logging.info("Modality {%s} was restore for user {%s}" % (modality, user))
+
+            sum_dimension += dimension
 
 
-fill_missed_modality(period=1)
+def store_modalities_in_one_vector(modalities, period):
+    db = connect_to_database(MONGO_HOST, MONGO_PORT, MONGO_DB_RESTORE)
+    users = db['MBTI_%d_%s' % (period, modalities[0])].distinct('_id')
+    for user in users:
+        user_vector = dict()
+        for modality in modalities:
+            modality_vector = db['MBTI_%d_%s' % (period, modality)].find_one({'_id': user})
+            del modality_vector['_id']
+            for feature in modality_vector:
+                user_vector["%s_%s" % (feature, modality)] = modality_vector[feature]
+        user_vector['_id'] = user
+        db['period_%d' % period].insert(user_vector)
+
+
+def divide_train_test_sets_disjoint(n_periods):
+    # find users, that posts in every window of [n_periods] periods
+    # 1 2 3 4 -> from this will return all users that posts in [1, 2], [3, 4].
+    # will give md5(user_id) for every user to avoid the duplication of ids
+    # will store all users in collections with names [train/test]_[n_periods]_[p from 1 to n_periods]
+    db = connect_to_database(MONGO_HOST, MONGO_PORT, MONGO_DB_RESTORE)
+
+    users_mapping = dict()  # map md5(user) -> user
+    all_users_groundtruth = dict()  # dict md5(user) -> MBTI
+    all_users = dict()  # dict md5(user) -> dict period -> features
+
+    # get all users
+    for i in range(1, PERIODS + 1):
+        if i + n_periods > PERIODS:
+            break
+        window_users = dict()
+        for p in range(0, n_periods):
+            period_users = db['period_%d' % (i + p)].find()
+            for user in period_users:
+                if user not in window_users:
+                    dict[user] = 1
+                else:
+                    dict[user] += 1
+
+        for user in window_users:
+            if window_users[user] != n_periods:
+                continue
+            new_id = hashlib.md5(user).hexdigest()
+            users_mapping[new_id] = user
+            all_users_groundtruth[new_id] = db['users'].find_one({'twitterUserName': user})['MBTI']
+            all_users[new_id] = dict()
+            for p in range(1, n_periods+1):
+                all_users[new_id][p] = db['period_%d'].find_one({'_id': user})
+
+        i += n_periods
+
+    # divide users into test and train
+    train_size, test_size = 0, 0
+    for user in all_users_groundtruth:
+        random = random.uniform(0, 1)
+        if random <= 0.8:
+            user['set'] = 'train'
+            train_size += 1
+        else:
+            user['set'] = 'test'
+            test_size += 1
+    logging.info("Train size [%d], test size [%d], total users [%d]" %
+                 train_size, test_size, train_size + test_size)
+    db['period_%d_users'].insert(all_users_groundtruth)
+    return
+
+
+def main(args):
+    periods = args[0]
+    for period in periods:
+        fill_missed_modality(period)
+        store_modalities_in_one_vector(['text', 'liwc', 'lda', 'location', 'media'], period)
+
+if __name__ == "__main__":
+    main([2, 3, 4, 5, 6, 7, 8, 9, 10])
+
 # period_data = get_period_data(2, 'tweets', 'tweets_10_periods')
 # input_data, output_data = input_output_generation(period_data, 2, 'users', 'user-profiling', 1)
 # train_input, train_output, test_input, test_output = split_data_to_train_test(input_data, output_data, 2)
